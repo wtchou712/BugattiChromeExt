85f35e4bfec8e36e3c134420dba9f4493ef669bc
Ted Wu <tedwu2016@u.northwestern.edu>
2015-11-22 11:21:29 -0600

    finalize PROJECT WOOOOOOO SUBMIT

--- js/popup.js
+++ js/popup.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     var email="thomas.yt.huang@gmail.com";
     $.get("http://instarito.azurewebsites.net/extension/thomas.yt.huang@gmail.com/favorites", function( data ) {
         food_favorites = data;
-        console.log("success");
-        console.log(food_favorites);
     });
+
+
 
     document.getElementById("submitbtn").addEventListener('click', function(){
         $(".loginform").remove();
@@ -14,29 +14,27 @@ $(document).ready(function() {
             var food_item = food_favorites[i].nickname;
             $('#main').append("<button class='ui-btn' id='" + id + "' >" + food_item + "</button>");
             addEvListener(id, food_item, email);
-            
         } 
     });
-
 });
+
 
 function addEvListener(id, food_item, email){
     var newlistener = document.getElementById(""+ id +"").addEventListener('click', function(){
-        minutes = 10;
-        var opt = {
-          type: "basic",
-          title: "Order Confirmation",
-          message: "Your order for " + food_item + " has been made and will arrive in " + minutes + " minutes.",
-          iconUrl: "icon.png"
-        };
-        chrome.notifications.create(opt, function(){});
-        request_delivery(email, food_item);
+        minutes = request_delivery(email, food_item);
         $("#main").append("<p>" + "Your order for " + food_item + " has been submitted</p>");
     });
 }
 
+function calculateMinutes(date) {
+    var hours = parseInt(date.substring(11,13));
+    var minutes = parseInt(date.substring(14,16));
+    var finalMinutes = (hours * 60) + minutes;
+    return finalMinutes;
+}
 
 function request_delivery(email, food_item) {
+    var minutesRemaining;
     $.ajax({
         type: 'POST',
         crossDomain: true,
@@ -51,13 +49,25 @@ function request_delivery(email, food_item) {
         },
         dataType: 'json',
         beforeSend: function( xhr ) {
-            // xhr.withCredentials = true;
-            // xhr.setRequestHeader("Authorization", "Basic " + btoa("03d2edd7-448b-4326-9ba6-7f465a683cae:"));
             xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
         },
         jsonpCallback :  'callback', 
-        success:function(data) {
-            console.log(data);
+        success: function(data) {
+            console.log("post data", data);
+            var minutesRemaining;
+            if(data.dropoff_eta == null){
+                minutesRemaining = calculateMinutes(data.dropoff_deadline) - calculateMinutes(data.created);
+            }
+            else{
+                minutesRemaining = calculateMinutes(data.dropoff_eta) - calculateMinutes(data.created);
+            }
+            var opt = {
+              type: "basic",
+              title: "Order Confirmation",
+              message: "Your order for " + food_item + " has been made and will arrive in " + minutesRemaining + " minutes.",
+              iconUrl: "icon.png"
+            };
+            chrome.notifications.create(opt, function(){});
         },
         error:function(data) {
             console.log("error");
