4b3697350eea7e45d0e7573b4a154c913252b392
Ted Wu <tedwu2016@u.northwestern.edu>
2015-11-22 02:35:45 -0600

    popup html

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,60 @@
 $(document).ready(function() {
-    for(var i=0; i < 3; i++) {
-        var id = i;
-        var buttonName= 'button1';
-        $('#main').append("<button class='food' id='" + id + "' >" + buttonName + i + "</button>");
-    }
-
-    chrome.browserAction.onClicked.addListener(function () {
-        $(".food").click(function() {
-            alert("id:" , id);
-        });
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        alert("hey");
     });
 
+    chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
+        if (tab.url.indexOf("https://www.google.co.in/") != -1) { // Inspect whether the place where user clicked matches with our list of URL
+            chrome.tabs.executeScript(tab.id, {
+                "file": "contentscript.js"
+            }, function () { // Execute your code
+                console.log("Script Executed .. "); // Notification on Completion
+            });
+        }
+    });
+    // for(var i=0; i < 3; i++) {
+    //     var id = i;
+    //     var buttonName= 'button';
+    //     $('#main').append("<button class='ui-btn' id='" + id + "' >" + buttonName + i + "</button>");
+    // }
+
+    // $(".ui-btn").click(function() {
+    //     // alert("id:" , id);
+    //     notifyMe();
+
+    // });
+
+    // // Request permission on page load
+    // document.addEventListener('DOMContentLoaded', function () {
+    //   if (Notification.permission !== "granted"){
+    //     Notification.requestPermission();
+    //     alert("notifications granted");
+    //   }
+    // });
+
+    // function notifyMe() {
+    //   if (!Notification) {
+    //     alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    //     return;
+    //   }
+
+    //   if (Notification.permission !== "granted")
+    //     Notification.requestPermission();
+    //   else {
+    //     var notification = new Notification('Notification title', {
+    //       icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+    //       body: "Hey there! You've been notified!",
+    //     });
+
+    //     notification.onclick = function () {
+    //       window.open("http://stackoverflow.com/a/13328397/1269037");      
+    //     };
+        
+    //   }
+
+    // }
 
     
 });
+
+
