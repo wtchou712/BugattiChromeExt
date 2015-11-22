96ebedcbe4d09b89b7c704f4bce658b2a8d8a70b
Ted Wu <tedwu2016@u.northwestern.edu>
2015-11-22 10:21:11 -0600

    ajax post requests

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,53 @@
 $(document).ready(function() {  
+    var food_favorites;
+    var email="thomas.yt.huang@gmail.com";
     $.get("http://instarito.azurewebsites.net/extension/thomas.yt.huang@gmail.com/favorites", function( data ) {
+        food_favorites = data;
         console.log("success");
+        console.log(food_favorites);
     });
+
     document.getElementById("submitbtn").addEventListener('click', function(){
         $(".loginform").remove();
-        for(var i = 0; i < 3; i++){
-            var id = i;
-            var food_item= 'button';
-            $('#main').append("<button class='ui-btn' id='" + id + "' >" + food_item + i + "</button>");
-            var newlistener = document.getElementById(""+ id +"").addEventListener('click', function(){
-                get_quote();
-                minutes = 10;
-                var opt = {
-                  type: "basic",
-                  title: "Order Confirmation",
-                  message: "Your order for " + food_item + " has been made and will arrive in " + minutes + " minutes.",
-                  iconUrl: "icon.png"
-                };
-                chrome.notifications.create(opt, function(){});
-                $("#main").append("<p>" + "Your order for " + food_item + " has been submitted</p>");
-            });
+        for(var i = 0; i < food_favorites.length; i++){
+            var id  = i;
+            var food_item = food_favorites[i].nickname;
+            $('#main').append("<button class='ui-btn' id='" + id + "' >" + food_item + "</button>");
+            addEvListener(id, food_item, email);
+            
         } 
     });
 
 });
 
+function addEvListener(id, food_item, email){
+    var newlistener = document.getElementById(""+ id +"").addEventListener('click', function(){
+        minutes = 10;
+        var opt = {
+          type: "basic",
+          title: "Order Confirmation",
+          message: "Your order for " + food_item + " has been made and will arrive in " + minutes + " minutes.",
+          iconUrl: "icon.png"
+        };
+        chrome.notifications.create(opt, function(){});
+        request_delivery(email, food_item);
+        $("#main").append("<p>" + "Your order for " + food_item + " has been submitted</p>");
+    });
+}
 
-function get_quote() {
+
+function request_delivery(email, food_item) {
     $.ajax({
         type: 'POST',
         crossDomain: true,
-        url: 'https://api.postmates.com/v1/customers/cus_K_2kliqDxo-EI-/delivery_quotes',
+        url: 'http://instarito.azurewebsites.net/order/' + email + '/' + food_item,
         headers: {
             "Authorization": "Basic " + btoa("e8c71ef3-a56c-40ce-be76-71113b198919:")
         },
         data: {
-            "api-key": "e8c71ef3-a56c-40ce-be76-71113b198919",
-            "username": "e8c71ef3-a56c-40ce-be76-71113b198919",
-            "password": "",
+            "email": email,
+            "nickname": food_item,
             "Content-Type": "application/json",
-            "dropoff_address": "20 McAllister St, San Francisco, CA 94102",
-            "pickup_address": "101 Market St, San Francisco, CA 94105"
         },
         dataType: 'json',
         beforeSend: function( xhr ) {
